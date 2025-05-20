65ea0a61b00e7898659f83672aa78fa53b871a57
PriyaBarjatia07 <priyabarjatia1280@gmail.com>
2025-05-20 17:20:15 +0530

    m

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import react from 'react'
 import ReactDOM from "react-dom/client";
-import { BrowserRouter,Routes,Route } from "react-router";
+import { BrowserRouter,Routes,Route } from "react-router-dom";
 
 
 import Admin from "./pages/admin";
